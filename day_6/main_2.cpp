3bbb773907a5cac9ad5360711310d35853bb8c9c
Yushua <yushabakker1@gmail.com>
2022-12-04 17:10:09 +0100

    day 6 p 2 done

--- day_6/main_2.cpp
+++ day_6/main_2.cpp
@@ -3,20 +3,78 @@
 #include <sstream>
 
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+std::string removeDuplicates(std::string string){
+    for ( std::string::size_type i = 0; i < string.size(); i++ ){
+        std::string::size_type j = i + 1;
+        while ( j < string.size() ){
+            if ( string[i] == string[j] ){
+                string.erase( j, 1 );
+            }
+            else{
+                ++j;
+            }
+        }
+    }
+    return (string);
+}
 
 int main(){
     std::ifstream infile("text_1.txt");
 
     std::string line;
 
-    int a;
-    int i;
-    int max = 0;
+    std::string string;
+
+    int score = 0;
+    std::string dupl;
+    int size = 0;
     while (std::getline(infile, line))
     {
+        std::string a;
         std::istringstream iss(line);
-        iss >> a;
+        if ((iss >> a)){
+            string += a;
+            size++;
+        }
+        else{
+            dupl = removeDuplicates(string);
+            std::cout << "[" << dupl << "][" << string << "]\n";
+            for (int y = 0; y < dupl.size(); y++){
+                int count = 0;
+                for (int i = 0; i < string.size(); i++)
+                    if (string[i] == dupl[y])
+                    count++;
+                std::cout << "[" << count << "][" << size << "]\n";
+                if (count == size){
+                    score += 1;
+                    std::cout << "char[" << dupl[y] << "]\n";
+                }
+            }
+            size = 0;
+            std::cout << "score [" << score << "]\n\n";
+            string = "";
+        }
     }
-    std::cout << "max [" << max << "]\n";
+    dupl = removeDuplicates(string);
+    std::cout << "[" << dupl << "][" << string << "]\n";
+    for (int y = 0; y < dupl.size(); y++){
+        int count = 0;
+        for (int i = 0; i < string.size(); i++)
+            if (string[i] == dupl[y])
+            count++;
+        std::cout << "[" << count << "][" << size << "]\n";
+        if (count == size){
+            score += 1;
+            std::cout << "char[" << dupl[y] << "]\n";
+        }
+    }
+    size = 0;
+    std::cout << "score [" << score << "]\n\n";
+    string = "";
+    std::cout << "score [" << score << "]\n";
     return (0);
 }
