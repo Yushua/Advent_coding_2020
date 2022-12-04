8e4099e4a88a6d74a787129f343e1d45ea80aa17
Yushua <yushabakker1@gmail.com>
2022-12-04 16:00:46 +0100

    adding day 3 p 1

--- day_3/main_1.cpp
+++ day_3/main_1.cpp
@@ -3,21 +3,40 @@
 #include <sstream>
 
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
 
 int main(){
     std::ifstream infile("text_1.txt");
-
     std::string line;
+    std::string a;
 
-    int a;
-    int i;
-    int max = 0;
+    std::list<std::string> forest;
     while (std::getline(infile, line))
     {
         std::istringstream iss(line);
         iss >> a;
+        forest.push_back(a);
     }
-    std::cout << "max [" << max << "]\n";
+    int posy = 0;
+    int posx = 0;
+    int pos = 0;
+    int score = 0;
+    for (std::string x : forest) {
+        std::cout << x << " " << pos << " " << posx << std::endl;
+        if (pos != 0){
+            if (posx >= x.length()){
+                posx -= x.length();
+            }
+            if (x[posx] == '#'){
+                score++;
+            }
+        }
+        posx+= 3;
+        pos++;
+	}
+    std::cout << score << std::endl;
     return (0);
 }
 
