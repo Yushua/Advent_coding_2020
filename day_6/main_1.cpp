d9194b972de78dd19704546c4dd160891fe46b91
Yushua <yushabakker1@gmail.com>
2022-12-04 16:57:40 +0100

    day 6, skipped the other tow because I already did them before

--- day_6/main_1.cpp
+++ day_6/main_1.cpp
@@ -3,21 +3,53 @@
 #include <sstream>
 
 #include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+std::string removeDuplicates(std::string string){
+    for ( std::string::size_type i = 0; i < string.size(); i++ ){
+        std::string::size_type j = i + 1;
+        while ( j < string.size() ){
+            if ( string[i] == string[j] ){
+                string.erase( j, 1 );
+            }
+            else{
+                ++j;
+            }
+        }
+    }
+    return (string);
+}
 
 int main(){
     std::ifstream infile("text_1.txt");
 
     std::string line;
 
-    int a;
-    int i;
-    int max = 0;
+    std::list<std::string> forest;
+    std::string string;
+
+    int score = 0;
     while (std::getline(infile, line))
     {
+        std::string a;
         std::istringstream iss(line);
-        iss >> a;
+        if ((iss >> a)){
+            string += a;
+        }
+        else{
+            string = removeDuplicates(string);
+            std::cout << "[" << string << "]\n";
+            score += string.length();
+            string = "";
+        }
     }
-    std::cout << "max [" << max << "]\n";
+    string = removeDuplicates(string);
+    std::cout << "[" << string << "]\n";
+    score += string.length();
+    string = "";
+    std::cout << "[" << score << "]\n";
     return (0);
 }
 
