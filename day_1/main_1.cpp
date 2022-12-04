339ad36dabe49e784eaaf8f5375fdbd445051859
Yushua <yushabakker1@gmail.com>
2022-12-04 15:23:53 +0100

    advent 2020 day 1 p 1

---
+++ day_1/main_1.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+int main(){
+    std::ifstream infile("text_1.txt");
+
+    std::string line;
+
+    int a;
+    std::list<int> my_list;
+    int max = 0;
+    while (std::getline(infile, line))
+    {
+        std::istringstream iss(line);
+        iss >> a;
+        my_list.push_back(a);
+    }
+    for (int x : my_list) {
+        for (int z : my_list){
+            if (x + z == 2020){
+                std::cout << "[" << x * z << "] " << x << " " << z << '\n';
+                return (0);
+            }
+        }
+	}
+    return (0);
+}
+
